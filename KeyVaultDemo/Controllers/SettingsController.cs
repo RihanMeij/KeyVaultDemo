7b320ee56463e6000bd21b2f5914e0d6474dfb37
Rihan Meij <rihan.meij@axsesstoday.com.au>
2018-11-28 09:22:56 +0200

    adding files

---
+++ KeyVaultDemo/Controllers/SettingsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KeyVaultDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KeyVaultDemo.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save(ExampleViewModel data)
+        {
+            System.Diagnostics.Debug.Write(data);
+            return Ok();
+        }
+    }
+}
---
+++ KeyVaultDemo/Models/ExampleViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeyVaultDemo.Models
+{
+    public class ExampleViewModel
+    {
+        public ExampleViewModel()
+        {
+            People = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
