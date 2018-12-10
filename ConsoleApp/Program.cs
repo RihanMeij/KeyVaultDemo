0cf09ca1d609ee00c6acd08167309279135af8af
Rihan Meij <rihan.meij@axsesstoday.com.au>
2018-12-10 07:43:17 +0200

    Final demo commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,19 +2,18 @@
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace ConsoleApp
 {
     class Program
     {
-        private const string EncryptionKeyUrl = "https://rihandemocptug.vault.azure.net/keys/DemoEncryption";
+        private const string EncryptionKeyUrl = "https://rihandemocptug.vault.azure.net/keys/Demo2";
         private const string EncryptionAlgorithm = "RSA-OAEP";
         private static async Task<string> GetToken(string authority, string resource, string scope)
         {
             var authContext = new AuthenticationContext(authority);
-            var clientCred = new ClientCredential("notepad","notepad");
+            var clientCred = new ClientCredential("key","secret");
             var result = await authContext.AcquireTokenAsync(resource, clientCred);
 
             return result.AccessToken;
@@ -27,9 +26,10 @@ namespace ConsoleApp
 
                 var input = Console.ReadLine();
                 var inputBytes = Encoding.ASCII.GetBytes(input);
+
                 var encryptedBytes = keyVault.EncryptAsync(EncryptionKeyUrl, EncryptionAlgorithm, inputBytes).Result.Result;
 
-                var encryptedString = Encoding.ASCII.GetString(encryptedBytes);
+                var encryptedString = Convert.ToBase64String(encryptedBytes);
                 Console.WriteLine(encryptedString);
 
                 var decryptedBytes = keyVault.DecryptAsync(EncryptionKeyUrl, EncryptionAlgorithm, encryptedBytes);
@@ -43,3 +43,37 @@ namespace ConsoleApp
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//var clientCred = new ClientCredential("ff79870a-7c6f-40bf-8706-32b887787af7","D3sfJxqZn27S7T/H1s5BGO50OOc87u4hgY2+yQyXbmI=");
